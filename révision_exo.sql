366c1c45f2d32f3f9baee003f8629a2b516f88c9
Enzo Mauclair <enzo.mauclair@gmail.com>
2024-10-03 14:59:49 +0200

    question 5

--- révision_exo.sql
+++ révision_exo.sql
@@ -5,3 +5,5 @@ SELECT nom, email FROM Clients;
 SELECT * FROM Clients WHERE age > 40;
 
 SELECT * FROM Clients WHERE location = 'Paris' AND age BETWEEN 25 AND 35;
+
+SELECT * FROM Campagnes ORDER BY budget;
